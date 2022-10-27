ebd5196f6b20363bcbac1e083db6be34b79bbd25
Tanvi <tanvi221110@gmail.com>
2022-10-27 15:09:53 +0530

    updated checkout attribute

--- src/Checkout.js
+++ src/Checkout.js
@@ -18,7 +18,7 @@ function Checkout() {
           <h2 className='checkout__title' >Your shopping Basket</h2>
          
          {basket.map(item => (
-          <Checkout id={item.id}
+          <CheckoutProduct id={item.id}
           title={item.title}
           image={item.image}
           price={item.price}
